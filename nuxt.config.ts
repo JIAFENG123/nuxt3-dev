c52b8867e566d7e526f86823165452bbc7999224
fjia <fjia@suntekcorps.com>
2023-01-10 18:07:50 +0800

    fix: test content not found

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,4 +24,7 @@ export default defineNuxtConfig({
       }),
     ],
   },
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,7 +12,7 @@ definePageMeta({
     <div>
       <div class="max-w-md py-4 px-8 bg-white dark:bg-[#333] shadow-lg rounded-lg my-20">
         <div class="flex justify-center md:justify-end -mt-16">
-          <img class="w-20 h-20 object-cover rounded-full border-2 border-green-500" src="../public/nuxt.png">
+          <img class="w-20 h-20 object-cover rounded-full border-2 border-green-500" src="/nuxt.png">
         </div>
         <div>
           <h2 class="text-gray-800 dark:text-gray-300 text-3xl font-semibold">
